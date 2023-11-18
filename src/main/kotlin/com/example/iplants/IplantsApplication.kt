b002cf22432aeb6d40a3a8ea581acf8a6716e9cc
JEYSON2403 <115761600+JEYSON2403@users.noreply.github.com>
2023-11-17 21:00:21 -0500

    Primer commit

---
+++ src/main/kotlin/com/example/iplants/IplantsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.iplants
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IplantsApplication
+
+fun main(args: Array<String>) {
+	runApplication<IplantsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/iplants/IplantsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.iplants
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IplantsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
